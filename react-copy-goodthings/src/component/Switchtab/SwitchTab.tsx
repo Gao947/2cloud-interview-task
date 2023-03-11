029631c79b2ebfbb2073e490ba38bdf5be33cb13
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-11 22:16:34 +1100

    use questionItem component to optimise question component

--- react-copy-goodthings/src/component/Switchtab/SwitchTab.tsx
+++ react-copy-goodthings/src/component/Switchtab/SwitchTab.tsx
@@ -1,40 +1,39 @@
-import React from "react";
 import { Container } from "react-bootstrap";
 import styles from "./SwitchTab.module.css";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
-function SwitchTabComponent (){
-    return (
-        <Container>
-        <Tabs
-      defaultActiveKey="profile"
-      id={styles['switch-tab-text']}
-      className="mb-3"
-      fill
-    >
-      <Tab eventKey="lastest" title="Latest Products"> 
-      Latest Products 
-      </Tab>
+function SwitchTabComponent() {
+  return (
+    <Container>
+      <Tabs
+        defaultActiveKey="profile"
+        id={styles['switch-tab-text']}
+        className="mb-3"
+        fill
+      >
+        <Tab eventKey="lastest" title="Latest Products">
+          Latest Products
+        </Tab>
 
-      <Tab eventKey="sellers" title="Best Sellers">
-      Best Sellers
-      </Tab>
+        <Tab eventKey="sellers" title="Best Sellers">
+          Best Sellers
+        </Tab>
 
-      <Tab eventKey="express" title="Express Products">
-      Express Products
-      </Tab>
+        <Tab eventKey="express" title="Express Products">
+          Express Products
+        </Tab>
 
-      <Tab eventKey="clothing" title="Clothing & Apparel">
-      Clothing & Apparel
-      </Tab>
+        <Tab eventKey="clothing" title="Clothing & Apparel">
+          Clothing & Apparel
+        </Tab>
 
-      <Tab eventKey="eco" title="Eco Friendly">
-      Eco Friendly Products
-      </Tab>
-    </Tabs>
-      </Container>
-    )
+        <Tab eventKey="eco" title="Eco Friendly">
+          Eco Friendly Products
+        </Tab>
+      </Tabs>
+    </Container>
+  )
 }
 
 export default SwitchTabComponent;
---
+++ react-copy-goodthings/src/data/questions.js
@@ -0,0 +1,67 @@
+export const questionData = [
+    {
+        "eventKey":"0",
+        "question": "What services does Good Things provide?",
+        "answer": "We have a fully-fledged team that can help you choose promotional products. We have a range of promotional product & marketing services including finding eco-friendly products, unique product ideas, artwork graphic design, custom packaging and delivery of your products. Our design accounts team will provide mock-ups and final proofs for you to approve before getting a branded sample made and sent your way."
+    },
+    {
+        "eventKey":"1",
+        "question": "Why should I use promotional products?",
+        "answer": "Promotional products are functional and usable pieces of advertising in the possession of your customers. Almost every household will have a branded tote bag or branded drink bottles that they regularly use. Whether it’s branded plastic pens, branded tote bags, branded notebooks or something more premium like custom golf umbrellas, high-quality branded portable chargers and branded reusable coffee cups — there is something for everyone when it comes to promotional gift ideas. If you want something that’s better for the environment, then look at our eco-friendly promotional products — better for you, better for your business and better for Earth."
+    },
+    {
+        "eventKey":"2",
+        "question": "What are your most popular promotional products?",
+        "answer": "Currently, our custom corduroy caps and custom designed socks are very popular promotional product items. If you want something more subtle then we recommend you go with branded bottles of water, custom bags, custom t-shirts, pens or even personal care items such as branded hand sanitiser and masks, sports towels and sunscreen. If you’re looking for something more work-related then we highly recommend promotional products and corporate gifts, including corporate stationary and Work from Home or care package."
+    },
+    {
+        "eventKey":"3",
+        "question": "Are your products Australian made?",
+        "answer": "Yes, some of our promotional products are made in Australia! We’re proud to be working with both on-shore Australian-wide suppliers as well as off-shore international suppliers. If you’re looking for Australian-made promotional merchandise only, then get in contact with our team and we can help you look for them."
+    },
+    {
+        "eventKey":"4",
+        "question": "How long does it take to get a quote?",
+        "answer": "We understand that your order enquiry is top priority. One of our key values is “Back in a flash”, our Sales team usually responds to your enquiry within 1 business day."
+    },
+    {
+        "eventKey":"5",
+        "question": "What format should I supply my artwork in?",
+        "answer": "You can supply your artwork in a wide range of formats. We highly recommend supplying your digital artwork in either AI (Adobe Illustrator), jpeg, png or PDF formats."
+    },
+    {
+        "eventKey":"6",
+        "question": "What is a vector file?",
+        "answer": "A vector file is a type of file that allows our design team to directly edit your artwork; this is usually through an .eps or Adobe Illustrator file. We highly recommend you read our Guide to Merchandise Lingo blog for more information about this."
+    },
+    {
+        "eventKey":"7",
+        "question": "Can you create custom packaging for a gift pack or product order?",
+        "answer": "Yes, we love creating custom packaging and branded boxes for your branded promotional products! This adds an additional dimension to your customer/employee journey. We can also design the artwork for your custom packaging."
+    },
+    {
+        "eventKey":"8",
+        "question": "Can I track my order from production right through to delivery?",
+        "answer": "Our expert team will keep you updated every step of the way. You will then receive a tracking number once your promotional products  have been shipped."
+    },
+    {
+        "eventKey":"9",
+        "question": "How long does delivery take?",
+        "answer": "Australian, on-shore delivery — delivery within 7 business days. Off-shore delivery — once samples are approved turnaround time and delivery is within 5 - 6 weeks airmail and 8 - 12 weeks cargo (sea) shipping. Due to COVID-19, we are experiencing delays and apologise in-advance. Please be patient with our team as we prioritise your order."
+    },
+    {
+        "eventKey":"10",
+        "question": "Do you ship worldwide?",
+        "answer": "Yes, we can ship your branded products across the globe as long as our courier service is available in your desired area. Please get in contact with us if you have any specific{ questions about shipping locations; we have a wide range of shipping options."
+    },
+    {
+        "eventKey":"11",
+        "question": "What if I receive more or less units than my order?",
+        "answer": "If you have received more units in your order than what was agreed on, then you are welcome to keep them at no extra cost! If however, you receive less units than what was signed off, our team will either make additional or credit you for the missing products."
+    },
+    {
+        "eventKey":"12",
+        "question": "I’m unsatisfied with my order, can I get a refund?",
+        "answer": "Unfortunately, we do not provide refunds to orders, as we cannot reuse branded promotional products. However, if your order does not match what was signed off then our team will be happy to discuss with you other options including a discount, replacement or refund depending on the extent of the issue."
+    }
+]
