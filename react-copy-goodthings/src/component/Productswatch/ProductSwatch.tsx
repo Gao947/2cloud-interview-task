f84ff2c9498057dd0ccddd8cadc0b81002c2f934
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-12 22:29:34 +1100

    Fix: Swatch Product buttons shape

--- react-copy-goodthings/src/component/Productswatch/ProductSwatch.tsx
+++ react-copy-goodthings/src/component/Productswatch/ProductSwatch.tsx
@@ -25,82 +25,78 @@ function ProductSwatchComponent() {
                 <Row>
                     <h2 className={styles['swatch-title']}>
                         Our Brandable Products
-                    </h2>   
+                    </h2>
                 </Row>
-
                 <Row>
                     <Row>
-                    <SwatchItemLarge 
-                        imageurl={productswatch1}
-                        hyperlink="https://goodthings.com.au/product-category/stationery/"
-                        originaltext="good notepads"
-                        hovertext="Great for conferences and events" />
+                        <SwatchItemLarge
+                            imageurl={productswatch1}
+                            hyperlink="https://goodthings.com.au/product-category/stationery/"
+                            originaltext="good notepads"
+                            hovertext="Great for conferences and events" />
 
                         <Col xl="6">
                             <Row>
-                            <SwatchItemLarge 
-                        imageurl={productswatch21}
-                        hyperlink="https://goodthings.com.au/product-category/promotional-bags/"
-                        originaltext="good bags"
-                        hovertext="Eco-friendly, cost effective and stylish, we've got it all in the bag." />
+                                <SwatchItemLarge
+                                    imageurl={productswatch21}
+                                    hyperlink="https://goodthings.com.au/product-category/promotional-bags/"
+                                    originaltext="good bags"
+                                    hovertext="Eco-friendly, cost effective and stylish, we've got it all in the bag." />
 
-<SwatchItemLarge 
-                        imageurl={productswatch22}
-                        hyperlink="https://goodthings.com.au/product-category/clothing-apparel/"
-                        originaltext="good t-shirts"
-                        hovertext="Wear your brand on your sleeve for all your promotional and event needs." />
+                                <SwatchItemLarge
+                                    imageurl={productswatch22}
+                                    hyperlink="https://goodthings.com.au/product-category/clothing-apparel/"
+                                    originaltext="good t-shirts"
+                                    hovertext="Wear your brand on your sleeve for all your promotional and event needs." />
                             </Row>
                             <Row>
-                            <SwatchItemLarge 
-                        imageurl={productswatch23}
-                        hyperlink="https://goodthings.com.au/product-category/drink-water-bottles/"
-                        originaltext="good bottles"
-                        hovertext="Your audience won't forget your brand, or to hydrate." />
-                          <SwatchItemLarge 
-                        imageurl={productswatch24}
-                        hyperlink="https://goodthings.com.au/product-category/promotional-pens/"
-                        originaltext="good pens"
-                        hovertext="Our personalised pens are something to write home about." />
+                                <SwatchItemLarge
+                                    imageurl={productswatch23}
+                                    hyperlink="https://goodthings.com.au/product-category/drink-water-bottles/"
+                                    originaltext="good bottles"
+                                    hovertext="Your audience won't forget your brand, or to hydrate." />
+                                <SwatchItemLarge
+                                    imageurl={productswatch24}
+                                    hyperlink="https://goodthings.com.au/product-category/promotional-pens/"
+                                    originaltext="good pens"
+                                    hovertext="Our personalised pens are something to write home about." />
                             </Row>
                         </Col>
                     </Row>
                     <Row>
                         <Col xl="6">
                             <Row>
-                            <SwatchItemLarge 
-                        imageurl={productswatch31}
-                        hyperlink="https://goodthings.com.au/product-category/clothing-apparel/"
-                        originaltext="good apparels"
-                        hovertext="When looking good matters" />
-                            <SwatchItemLarge 
-                        imageurl={productswatch32}
-                        hyperlink="https://goodthings.com.au/product-category/clothing-apparel/custom-socks/"
-                        originaltext="good socks"
-                        hovertext="Slip into our soft cotton (or wool) socks" />
+                                <SwatchItemLarge
+                                    imageurl={productswatch31}
+                                    hyperlink="https://goodthings.com.au/product-category/clothing-apparel/"
+                                    originaltext="good apparels"
+                                    hovertext="When looking good matters" />
+                                <SwatchItemLarge
+                                    imageurl={productswatch32}
+                                    hyperlink="https://goodthings.com.au/product-category/clothing-apparel/custom-socks/"
+                                    originaltext="good socks"
+                                    hovertext="Slip into our soft cotton (or wool) socks" />
                             </Row>
                             <Row>
-                            <SwatchItemLarge 
-                        imageurl={productswatch33}
-                        hyperlink="https://goodthings.com.au/product-category/technology/"
-                        originaltext="good tech"
-                        hovertext="Functional and useful technology" />
-                            <SwatchItemLarge 
-                        imageurl={productswatch34}
-                        hyperlink="https://goodthings.com.au/product-category/branded-umbrellas/"
-                        originaltext="good umbrellas"
-                        hovertext="Always be ready for a rainy day" />
+                                <SwatchItemLarge
+                                    imageurl={productswatch33}
+                                    hyperlink="https://goodthings.com.au/product-category/technology/"
+                                    originaltext="good tech"
+                                    hovertext="Functional and useful technology" />
+                                <SwatchItemLarge
+                                    imageurl={productswatch34}
+                                    hyperlink="https://goodthings.com.au/product-category/branded-umbrellas/"
+                                    originaltext="good umbrellas"
+                                    hovertext="Always be ready for a rainy day" />
                             </Row>
                         </Col>
-
-                        <SwatchItemLarge 
-                        imageurl={productswatch4}
-                        hyperlink="https://goodthings.com.au/product-category/caps-hats-clothing-apparel/"
-                        originaltext="good hats"
-                        hovertext="Promotional hats will cap-tivate your audience." />
-
+                        <SwatchItemLarge
+                            imageurl={productswatch4}
+                            hyperlink="https://goodthings.com.au/product-category/caps-hats-clothing-apparel/"
+                            originaltext="good hats"
+                            hovertext="Promotional hats will cap-tivate your audience." />
                     </Row>
                 </Row>
-
                 <Row>
                     <Col>
                         <Button id={styles['swatch-button-left']} variant="primary">Browse all products</Button>
