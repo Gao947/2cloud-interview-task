5f8d2340a15341070641ff23a252db9b3609ada7
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-13 08:55:38 +1100

    Add switch tab text

--- react-copy-goodthings/src/component/Switchtab/SwitchTab.tsx
+++ react-copy-goodthings/src/component/Switchtab/SwitchTab.tsx
@@ -5,7 +5,8 @@ import Tabs from 'react-bootstrap/Tabs';
 import { useKeenSlider } from 'keen-slider/react' 
 import Image from 'react-bootstrap/Image';
 
-import lp12 from "../../img/LP12.webp";
+import lp1 from "../../img/LP1.webp";
+import lp2 from "../../img/LP2.webp";
 import lp3 from "../../img/LP3.webp";
 import lp4 from "../../img/LP4.webp";
 import lp5 from "../../img/LP5.webp";
@@ -33,28 +34,44 @@ function SwitchTabComponent() {
         <Tab eventKey="lastest" title="Latest Products">
     <div ref={sliderRef} className="keen-slider">
       <div className="keen-slider__slide number-slide1">
-      <Image id={styles['switch-img-container']} src={lp12} />
+      <Image id={styles['switch-img-container']} src={lp1} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide2">
-      <Image id={styles['switch-img-container']} src={lp12} />
+      <Image id={styles['switch-img-container']} src={lp2} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide3">
       <Image id={styles['switch-img-container']} src={lp3} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide4">
       <Image id={styles['switch-img-container']} src={lp4} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide5">
       <Image id={styles['switch-img-container']} src={lp5} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide6">
       <Image id={styles['switch-img-container']} src={lp6} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide6">
       <Image id={styles['switch-img-container']} src={lp7} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
       <div className="keen-slider__slide number-slide6">
       <Image id={styles['switch-img-container']} src={lp8} />
+      <span>NSW Touch Football Bilk Shorts</span>
+      <span>from$31.8</span> <span>Min Qty: 1</span>
       </div>
     </div>
         </Tab>
