b5f099bcb178a57d2791ea08d677247b4f447d76
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-12 11:07:32 +1100

    fix: Carousel slide background colour

--- react-copy-goodthings/src/component/Carousel/Carousel.tsx
+++ react-copy-goodthings/src/component/Carousel/Carousel.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container } from "react-bootstrap";
 import styles from "./Carousel.module.css";
 import Carousel from 'react-bootstrap/Carousel';
@@ -9,66 +8,75 @@ import carousel4 from "../../img/slider-appawinner.webp";
 
 
 
-function CarouselComponent (){
-    return (
-        <Container>
- <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carousel1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carousel2}
-          alt="Second slide"
-        />
+function CarouselComponent() {
+  return (
+    <div>
+        <Carousel>
+          <Carousel.Item id={styles['first-slide']} className={styles['wide-screen-slide']}>
+          <Container>
+            <img
+              className="d-block w-100"
+              src={carousel1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+            </Container>
+          </Carousel.Item>
+          <Carousel.Item id={styles['second-slide']} className={styles['wide-screen-slide']}>
+          <Container>
+            <img
+              className="d-block w-100"
+              src={carousel2}
+              alt="Second slide"
+            />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carousel3}
-          alt="Third slide"
-        />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+            </Container>
+          </Carousel.Item>
+          <Carousel.Item id={styles['third-slide']} className={styles['wide-screen-slide']}>
+          <Container>
+            <img
+              className="d-block w-100"
+              src={carousel3}
+              alt="Third slide"
+            />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+            </Container>
+          </Carousel.Item>
 
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carousel4}
-          alt="Fourth slide"
-        />
+          <Carousel.Item id={styles['fourth-slide']} className={styles['wide-screen-slide']} >
+            <Container>
+            <img
+              className="d-block w-100"
+              src={carousel4}
+              alt="Fourth slide"
+            />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+            </Container>
+          </Carousel.Item>
 
-    </Carousel>
-      </Container>
-    )
+        </Carousel>
+      
+    </div>
+  )
 }
 
 export default CarouselComponent;
