f58c1e38e52156062ef3195ff28055b219b43bff
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-12 23:21:57 +1100

    WIP: SwitchTab component

--- react-copy-goodthings/src/component/Switchtab/SwitchTab.tsx
+++ react-copy-goodthings/src/component/Switchtab/SwitchTab.tsx
@@ -3,19 +3,60 @@ import styles from "./SwitchTab.module.css";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import { useKeenSlider } from 'keen-slider/react' 
+import Image from 'react-bootstrap/Image';
 
-
+import lp12 from "../../img/LP12.webp";
+import lp3 from "../../img/LP3.webp";
+import lp4 from "../../img/LP4.webp";
+import lp5 from "../../img/LP5.webp";
+import lp6 from "../../img/LP6.webp";
+import lp7 from "../../img/LP7.webp";
+import lp8 from "../../img/LP8.webp";
 
 
 function SwitchTabComponent() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 4,
+      spacing: 8,
+    },
+  })
+
   return (
-    <Container className={styles['swatch-tab-body']}>
+    <div className={styles['swatch-tab-body']}>
+    <Container>
       <Tabs
         defaultActiveKey="profile"
         className={styles['switch-tab-text']}
       >
         <Tab eventKey="lastest" title="Latest Products">
-          Latest Products
+    <div ref={sliderRef} className="keen-slider">
+      <div className="keen-slider__slide number-slide1">
+      <Image id={styles['switch-img-container']} src={lp12} />
+      </div>
+      <div className="keen-slider__slide number-slide2">
+      <Image id={styles['switch-img-container']} src={lp12} />
+      </div>
+      <div className="keen-slider__slide number-slide3">
+      <Image id={styles['switch-img-container']} src={lp3} />
+      </div>
+      <div className="keen-slider__slide number-slide4">
+      <Image id={styles['switch-img-container']} src={lp4} />
+      </div>
+      <div className="keen-slider__slide number-slide5">
+      <Image id={styles['switch-img-container']} src={lp5} />
+      </div>
+      <div className="keen-slider__slide number-slide6">
+      <Image id={styles['switch-img-container']} src={lp6} />
+      </div>
+      <div className="keen-slider__slide number-slide6">
+      <Image id={styles['switch-img-container']} src={lp7} />
+      </div>
+      <div className="keen-slider__slide number-slide6">
+      <Image id={styles['switch-img-container']} src={lp8} />
+      </div>
+    </div>
         </Tab>
 
         <Tab eventKey="sellers" title="Best Sellers">
@@ -35,6 +76,7 @@ function SwitchTabComponent() {
         </Tab>
       </Tabs>
     </Container>
+    </div>
   )
 }
 
