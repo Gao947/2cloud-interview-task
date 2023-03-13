fcafe2d44fa13761afecbe87bd71499c2ef00951
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-13 20:43:52 +1100

    Fix: NavBar Component

--- react-copy-goodthings/src/component/Navbar/NavBar.tsx
+++ react-copy-goodthings/src/component/Navbar/NavBar.tsx
@@ -1,121 +1,167 @@
 import React, { useState } from "react";
-import { Dropdown } from "react-bootstrap";
+import { Button, Col, Dropdown, Row } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from "./NavBar.module.css";
+// import { Arrow90degRight }from 'react-bootstrap-icons';
 
 function NavBar() {
-    const [showDropdown, setShowDropdown] = useState(false);
+    const [showAllProductDropdown, setShowAllProductDropdown] = useState(false);
+    const [showBagsDropdown, setShowBagsDropdown] = useState(false);
+    const [showConfectioneryDropdown, setShowConfectioneryDropdown] = useState(false);
+    const [showDrinkBottlesDropdown, setShowDrinkBottlesDropdown] = useState(false);
+    const [showClothingAndApparelDropdown, setShowClothingAndApparelDropdown] = useState(false);
+    const [showEcoFriendlyDropdown, setShowEcoFriendlyDropdown] = useState(false);
+    const [showContactUsDropdown, setShowContactUsDropdown] = useState(false);
+
     return (
         <div>
             <Navbar className={styles['navbar']}>
                 <Container>
-                            <NavDropdown
-                                title="All products"
-                                onMouseLeave={() => setShowDropdown(false)}
-                                onMouseOver={() => setShowDropdown(true)}
-                                id={styles['navbar-button']}
-                                show={showDropdown}
-                            >
-                                <NavDropdown.Item href="#action/3.1"> 🌟 Best Sellers 🌟 </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.2"> Another action </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3"> Something </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown
-                                title="bags"
-                                onMouseLeave={() => setShowDropdown(false)}
-                                onMouseOver={() => setShowDropdown(true)}
-                                id={styles['navbar-button']}
-                                show={showDropdown}
-                            >
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown
-                                title="confectionery"
-                                onMouseLeave={() => setShowDropdown(false)}
-                                onMouseOver={() => setShowDropdown(true)}
-                                id={styles['navbar-button']}
-                                show={showDropdown}
-                            >
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                    <NavDropdown
+                        title="All products"
+                        onMouseLeave={() => setShowAllProductDropdown(false)}
+                        onMouseOver={() => setShowAllProductDropdown(true)}
+                        id={styles['navbar-item']}
+                        show={showAllProductDropdown}
+                        className={styles['mega-menu-dropdown']}
+                    >
+                        <Container className={styles['menu-col-item']}>
+                            <div id={styles['menu-col-item-text']}>
+                                <NavDropdown.Item href="#"> 🌟 Best Sellers 🌟 </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Bespoke Products </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Caps & Hats </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Clothing & Apparel </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Conferences & Events </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Customer Mugs </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> EcoFriendly </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Gift Sets </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Homeware </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Outdoor </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Personal Care </NavDropdown.Item>
+                            </div>
+                        </Container>
+                    </NavDropdown>
+                    <NavDropdown
+                        title="bags"
+                        onMouseLeave={() => setShowBagsDropdown(false)}
+                        onMouseOver={() => setShowBagsDropdown(true)}
+                        id={styles['navbar-item']}
+                        show={showBagsDropdown}
+                        className={styles['mega-menu-dropdown']}
+                    >
+                        <Container className={styles['menu-col-item']}>
+                            <div id={styles['menu-col-item-text']}>
+                                <NavDropdown.Item href="#"> Tote Bags </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Wine Bags </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Bag Tags </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Paper Bags </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Cooler Bags </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Backpacks & Backsacks </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Laptop Bags </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Satchels </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Duffle & Sports Bags </NavDropdown.Item>
+                            </div>
+                        </Container>
+                    </NavDropdown>
+                    <NavDropdown
+                        title="confectionery"
+                        onMouseLeave={() => setShowConfectioneryDropdown(false)}
+                        onMouseOver={() => setShowConfectioneryDropdown(true)}
+                        id={styles['navbar-item']}
+                        show={showConfectioneryDropdown}
+                    >
+                        <Container className={styles['menu-col-item']}>
+                            <div id={styles['menu-col-item-text']}>
+                                <NavDropdown.Item href="#"> Sklttles </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Promotional Chocolate </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Promotional Jelly Beans </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Promotional Lolly Pops </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Mints </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Mixed Lollies </NavDropdown.Item>
+                            </div>
+                        </Container>
+                    </NavDropdown>
+                    <NavDropdown
+                        title="drink bottles"
+                        onMouseLeave={() => setShowDrinkBottlesDropdown(false)}
+                        onMouseOver={() => setShowDrinkBottlesDropdown(true)}
+                        id={styles['navbar-item']}
+                        show={showDrinkBottlesDropdown}
+                    >
+                        <Container className={styles['menu-col-item']}>
+                            <div id={styles['menu-col-item-text']}>
+                                <NavDropdown.Item href="#"> Plastic Drink Bottles </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Metal Drink Bottles </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Double Wall Drink Bottle </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Glass Drink Bottles </NavDropdown.Item>
+                            </div>
+                        </Container>
+                    </NavDropdown>
+                    <NavDropdown
+                        title="clothing & apparel"
+                        onMouseLeave={() => setShowClothingAndApparelDropdown(false)}
+                        onMouseOver={() => setShowClothingAndApparelDropdown(true)}
+                        id={styles['navbar-item']}
+                        show={showClothingAndApparelDropdown}
+                    >
+                        <Container className={styles['menu-col-item']}>
+                            <div id={styles['menu-col-item-text']}>
+                                <NavDropdown.Item href="#"> T-shirts - Mens </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> T-shirts – Women’s </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Polo Shirts </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Custom Socks </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Singlets & Tanks </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Jackets & Vests </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Kids & Youth </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Corporate Wear </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Ties & Scarves </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Promotional Board Shorts </NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Aprons </NavDropdown.Item>
+                            </div>
+                        </Container>
+                    </NavDropdown>
+                    <NavDropdown
+                        title="eco friendly"
+                        onMouseLeave={() => setShowEcoFriendlyDropdown(false)}
+                        onMouseOver={() => setShowEcoFriendlyDropdown(true)}
+                        id={styles['navbar-item']}
+                        show={showEcoFriendlyDropdown}
+                    >
+                        <NavDropdown.Divider />
+                    </NavDropdown>
 
-                            <NavDropdown
-                                title="drink bottles"
-                                onMouseLeave={() => setShowDropdown(false)}
-                                onMouseOver={() => setShowDropdown(true)}
-                                id={styles['navbar-button']}
-                                show={showDropdown}
-                            >
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                    <Dropdown
+                        onMouseLeave={() => setShowContactUsDropdown(false)}
+                        onMouseOver={() => setShowContactUsDropdown(true)}
+                        style={{ width: '50px' }}
+                    >
+                        <Dropdown.Toggle
+                            className="main-style"
+                            id={styles['nabar-button']}
+                        >
+                            Contact us
+                        </Dropdown.Toggle>
 
-                            <NavDropdown
-                                title="clothing & apparel"
-                                onMouseLeave={() => setShowDropdown(false)}
-                                onMouseOver={() => setShowDropdown(true)}
-                                id={styles['navbar-button']}
-                                show={showDropdown}
-                            >
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-
-                            <NavDropdown
-                                title="eco friendly"
-                                onMouseLeave={() => setShowDropdown(false)}
-                                onMouseOver={() => setShowDropdown(true)}
-                                id={styles['navbar-button']}
-                                show={showDropdown}
-                            >
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-
-                            <Dropdown
-      onMouseLeave={() => setShowDropdown(false)}
-      onMouseOver={() => setShowDropdown(true)}
-      style={{ width: '166px' }}
-    >
-      <Dropdown.Toggle
-        className="main-style"
-        id="dropdown-basic"
-      >
-        Contact us
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu show={showDropdown}>
-        <Dropdown.Item href="#/action-1">
-          Action
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-2">
-          Another action
-        </Dropdown.Item>
-        <Dropdown.Item href="#/action-3">
-          Something else
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+                        <Dropdown.Menu show={showContactUsDropdown}>
+<Container id={styles['contact-item']}>
+    <Button id={styles['contact-button']}>Email us</Button>
+                            <Dropdown.Item href="#/action-1" id={styles['contact-text']}>
+                            <p>Please note most of our <br />
+                                products have a minimum qty of <br />
+                                20+ units. If you'd like to chat <br />
+                                call:
+                                </p>
+                            <a href="#/action-1"> 1800 659 649 </a>
+                            <p>For assistance with your order, <br />
+                            select Option 2.
+                            </p>
+                            </Dropdown.Item>
+                            </Container>
+                        </Dropdown.Menu>
+                    </Dropdown>
 
                 </Container>
             </Navbar>
