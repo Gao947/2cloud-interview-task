3d99e170302ee338a92d6eb51234dfaab7aae206
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-13 17:15:32 +1100

    Fix: Hellobar Text size

--- react-copy-goodthings/src/component/Hellobar/HelloBar.tsx
+++ react-copy-goodthings/src/component/Hellobar/HelloBar.tsx
@@ -4,9 +4,9 @@ import styles from "./HelloBar.module.css";
 
 function HelloBar (){
     return (
-        <div className={styles.Hellobar}>
-        <Container>
-          <span className={styles.text}>
+        <div className={styles['Hellobar']}>
+        <Container className={styles['text']}>
+          <span>
             Happy summer Australia!☀️ We're back and ready to help you with all your merch needs 😎
           </span>
         </Container>
