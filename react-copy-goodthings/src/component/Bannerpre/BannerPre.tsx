275c601cbccc886b5c10cb642e69d5502cc1c8e2
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-13 16:07:44 +1100

    Add bannerpre component tracelogo

--- react-copy-goodthings/src/component/Bannerpre/BannerPre.tsx
+++ react-copy-goodthings/src/component/Bannerpre/BannerPre.tsx
@@ -1,35 +1,39 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import styles from "./BannerPre.module.css";
 import { GlobeAsiaAustralia, Amd, Rocket, Lightbulb, Telephone } from 'react-bootstrap-icons';
+import Image from 'react-bootstrap/Image';
+import tracelogo from "../../img/trace-climate-positive.webp";
 
 function BannerPre() {
     return (
         <div className={styles.bannerpre}>
             <Container>
-                <div className={styles["banner-text"]}>
-                    <span className={styles["banner-item"]}>
+                <Row className={styles["banner-text"]}>
+                    <Col className={styles["banner-item"]}>
                         <GlobeAsiaAustralia className={styles["banner-icon"]} />
                         Australian owned
-                    </span>
-                    <span className={styles["banner-item"]}>
+                    </Col>
+                    <Col className={styles["banner-item"]}>
                         <Amd className={styles["banner-icon"]} />
                         Free artwork
-                    </span>
-                    <span className={styles["banner-item"]}>
+                    </Col>
+                    <Col className={styles["banner-item"]}>
                         <Lightbulb className={styles["banner-icon"]} />
                         Our protfolio
-                    </span>
-                    <span className={styles["banner-item"]}>
+                    </Col>
+                    <Col className={styles["banner-item"]}>
                         <Telephone className={styles["banner-icon"]} />
                         1800 659 649
-                    </span>
-                    <span className={styles["banner-item"]}>
+                    </Col>
+                    <Col className={styles["banner-item"]}>
                         <Rocket className={styles["banner-icon"]} />
                         About us
-                    </span>
-                    <span className={styles["banner-item"]}>Logo placeholder</span>
-                </div>
+                    </Col>
+                    <Col className={styles["banner-item"]}>
+                    <Image className={styles['trace-logo']} src={tracelogo} />
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
